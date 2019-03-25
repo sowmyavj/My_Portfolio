3fb1548edac1d3ef2f2f14d8618a4cf22dba0dce
sowmyavj <sowmyav@sowmyas-MacBook-Air.local>
2019-03-24 22:12:35 -0400

    Updates

---
+++ precache-manifest.a0ac925bb06861da8115af117e88e157.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a87ae0601e8b9388f124",
+    "url": "/My_Portfolio/static/css/main.5acd9560.chunk.css"
+  },
+  {
+    "revision": "a87ae0601e8b9388f124",
+    "url": "/My_Portfolio/static/js/main.cea15b4a.chunk.js"
+  },
+  {
+    "revision": "578ce61cda3234350f52",
+    "url": "/My_Portfolio/static/js/runtime~main.df9bda23.js"
+  },
+  {
+    "revision": "292d7f4989de181a1d44",
+    "url": "/My_Portfolio/static/js/2.14ac667d.chunk.js"
+  },
+  {
+    "revision": "04b114cb141641b30a99e1536b6e6a50",
+    "url": "/My_Portfolio/static/media/sowmya.04b114cb.jpg"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/My_Portfolio/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "06111c1882415264662ffb8943143526",
+    "url": "/My_Portfolio/static/media/github.06111c18.png"
+  },
+  {
+    "revision": "aad023ccf5e924689ee19181f922ee0f",
+    "url": "/My_Portfolio/static/media/Sowmya_Vijayakumar_resume.aad023cc.pdf"
+  },
+  {
+    "revision": "89c0c05ac0b8fc5235264dddc67cbbf9",
+    "url": "/My_Portfolio/index.html"
+  }
+];
